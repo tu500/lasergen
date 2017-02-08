5c8cb5b3bdc3bf19b95b581041e48b669bcbd963
Ike <ike@tura-home.de>
2017-02-08 16:55:59 +0100

    Minor makeup and comment

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config():
 
     layers = {
-            'cut': 'black',
-            'warn': 'red',
-            'info': 'green',
+            'cut'  : 'black',
+            'warn' : 'red',
+            'info' : 'green',
         }
 
     abort_on_tooth_length_error = False
@@ -13,6 +13,6 @@ class Config():
         self.tooth_min_width = tooth_min_width
         self.tooth_max_width = tooth_max_width
         self.wall_thickness = wall_thickness
-        self.subwall_thickness = wall_thickness
+        self.subwall_thickness = wall_thickness # other values aren't supported yet
         self.cutting_width = cutting_width
         self.object_distance = object_distance
