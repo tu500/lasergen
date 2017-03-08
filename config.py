cdfab068f501873cee58c1e212c5cc023f16b807
Ike <ike@koeln.ccc.de>
2017-03-08 01:36:50 +0100

    Implement copy for Config

--- config.py
+++ config.py
@@ -20,6 +20,25 @@ class Config():
         self.cutting_width = cutting_width
         self.object_distance = object_distance
 
+    def copy(self):
+
+        n = Config(
+                self.tooth_min_width,
+                self.tooth_max_width,
+                self.wall_thickness,
+                self.object_distance,
+                self.cutting_width,
+            )
+
+        n.subwall_thickness           = self.subwall_thickness
+        n.abort_on_tooth_length_error = self.abort_on_tooth_length_error
+        n.print_wall_names            = self.print_wall_names
+        n.warn_for_unclosed_paths     = self.warn_for_unclosed_paths
+
+        n.colors = self.colors.copy()
+
+        return n
+
     def get_color_from_layer(self, layer):
 
         if layer.warn_level is not None:
